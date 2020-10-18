1450d140afafc442859ea8aea8621b9fa04be9e0
BrandgrandReal <brandgrandbusiness@gmail.com>
2020-10-18 15:46:27 +0000

    V2.0

--- cmds/leave.js
+++ cmds/leave.js
@@ -2,13 +2,13 @@ var bowner = '531186390717825074';
 const mpmsg = `!!ERROR!!\nYou dont have the required perms!`
 module.exports = {
   catagory: 'util',
-  name: 'join',
-  desc: 'Simulates someone joining',
+  name: 'leave',
+  desc: 'Simulates someone leaving',
 execute: async (message, args, client, db, packageInfo, Discord, member) => {
- let chx = db.get(`welchannel_${message.guild.id}`);
+let chx = db.get(`byechannel_${message.guild.id}`);
  if (!client.channels.cache.get(chx).permissionsFor(message.author).has('SEND_MESSAGES') && !ids.includes(message.author.id)) return message.reply(mpmsg);
     let user = message.author;
  if (user) member = message.guild.member(user);
-   client.emit('guildMemberAdd', member)
+   client.emit('guildMemberRemove', member)
  if (message.deletable) return message.delete();
 }};
